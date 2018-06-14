47675d742c50dd5a75c09ea945052b62491da992
ronvree <ronvbree@gmail.com>
2018-06-14 10:33:29 +0200

    stub sacx

--- sacx/tasked_p_network.py
+++ sacx/tasked_p_network.py
@@ -1,17 +1,51 @@
+import keras as ks
+import numpy as np
+
 from core import State, Action
 from sacx.extcore import Task
 
 
 class PolicyNetwork:
 
+    def __init__(self,
+                 model_input: ks.layers.Input,
+                 model_outputs: dict,
+                 out_map,
+                 feature_ex,
+                 sa_map,
+                 ):
+
+        self.models = {task: ks.Model(inputs=[model_input], outputs=[output]) for task, output in model_outputs.items()}
+        self.out_map = out_map
+        self.fex = feature_ex
+        self.sa_map = sa_map
+
+        for model in self.models.values():
+            model.compile()  # TODO
+
     def distribution(self, state: State, task: Task) -> dict:
-        pass  # TODO
+        model = self.models[task]
+        actions = self.sa_map(state)
+        dist = {a: 0 for a in self.out_map}
+        values = model.predict([self.fex(state)])[0]
+        for a in actions:
+            dist[a] = values[self.out_map.index(a)]
+        total = sum(dist.values())
+        if total == 0:
+            return {a: 1 / len(actions) for a in dist.keys()}
+        else:
+            return {a: v / total for a, v in dist.items()}
 
     def train(self):
         pass  # TODO
 
     def sample(self, state: State, task: Task) -> Action:
-        pass  # TODO
+        return self._sample(self.distribution(state, task))
+
+    @staticmethod
+    def _sample(dist):
+        actions, probabilities = zip(*dist.items())
+        return np.random.choice(actions, p=probabilities)
 
     def sample_greedy(self, state: State, task: Task) -> Action:
         pass  # TODO
@@ -20,4 +54,5 @@ class PolicyNetwork:
         pass  # TODO
 
     def sample_distribution(self, state: State, task: Task) -> tuple:
-        pass  # TODO
+        dist = self.distribution(state, task)
+        return self._sample(dist), dist
