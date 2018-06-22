bb994e1fe2a925255ff9e56e07f743810c44fb98
Carl Beekhuizen <carl.beek@gmail.com>
2018-06-22 11:24:47 +0200

    Implements working logger for snek deep sarsa

--- cluster_experiments/snake_deep_sarsa.py
+++ cluster_experiments/snake_deep_sarsa.py
@@ -3,7 +3,7 @@ from multiprocessing import Process
 NUM_RUNS = 5  # Number of runs of each experiment over which will be averaged later
 
 
-def snake_deep_sarsa(run_num=0, file_name='snek'):
+def snake_deep_sarsa(episodes=5000, file_name='snek'):
     import keras as ks
     import numpy as np
     from experiment_util import Logger
@@ -11,7 +11,7 @@ def snake_deep_sarsa(run_num=0, file_name='snek'):
     from environments.snake import SnakeContinuous
     from q_network_sarsa_lambda import QNetworkSL
 
-    Logger(filename=file_name)
+    logger = Logger(filename=file_name)
 
     neural_network = ks.models.Sequential()
     neural_network.add(ks.layers.Dense(150, activation='relu', input_shape=(9,)))
@@ -20,7 +20,7 @@ def snake_deep_sarsa(run_num=0, file_name='snek'):
 
     neural_network.compile(optimizer=ks.optimizers.Adam(lr=0.001), loss='mse')
 
-    env = SnakeContinuous(render=True, render_freq=10)
+    env = SnakeContinuous(render=False, render_freq=10)
     actions = env.valid_actions()
 
     dqn = QNetworkSL(neural_network, actions, lambda x: np.reshape(x.state, newshape=(1, 9)),
@@ -33,15 +33,17 @@ def snake_deep_sarsa(run_num=0, file_name='snek'):
     dql = DeepSarsa(env, dqn,
                     epsilon=0.3,
                     epsilon_step_factor=0.999,
-                    epsilon_min=0.05,
+                    epsilon_min=0.02,
                     replay_memory_size=1000
                     )
-    q = dql.learn()
+    experiment = logger.start_experiment(dql.get_configuration())
+    q = dql.learn(num_episodes=episodes, result_handler=experiment.log)
+    experiment.save_attribute("weights", neural_network.get_weights())
 
 
 if __name__ == '__main__':
     jobs = [Process(target=snake_deep_sarsa,
-                    args=(i, 'snake_continuous_deep_sarsa_run_' + str(i) + '.h5')) for i in range(NUM_RUNS)]
+                    args=(5000, '../results/snake_continuous_deep_sarsa_run_' + str(i) + '.h5')) for i in range(NUM_RUNS)]
     for j in jobs:
         j.start()
     for j in jobs:
