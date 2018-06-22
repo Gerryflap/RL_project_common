a2c36183b109f4379553c1f665a8e73ece1c1360
gerben <gerry2211@gmail.com>
2018-06-22 15:40:38 +0200

    Made the cartpole deep sarsa run more parallel for cluster

--- cluster_experiments/cartpole_deepsarsalambda.py
+++ cluster_experiments/cartpole_deepsarsalambda.py
@@ -4,7 +4,7 @@ import time
 import os
 
 
-def experiment(runs, episodes, sigmas, lambda_parameter):
+def experiment(run_n, episodes, sigmas, lambda_parameter):
     import keras as ks
     import numpy as np
     from agents.deep_sarsa import DeepSarsa
@@ -13,42 +13,42 @@ def experiment(runs, episodes, sigmas, lambda_parameter):
 
     from experiment_util import Logger
 
-    filename = ("results/cartpole_deepsarsalambda_lambda_%1.2f.h5" %lambda_parameter)
+    filename = ("../results/cartpole_deepsarsalambda_lambda_%1.2f_%d.h5" %(lambda_parameter, run_n))
     l = Logger(filename= filename)
 
-    for run_n in range(runs):
-        for sigma in sigmas:
-            
-            neural_network = ks.models.Sequential()
-            neural_network.add(ks.layers.Dense(150, activation='relu', input_shape=(4,)))
-            neural_network.add(ks.layers.Dense(50, activation='relu'))
-            neural_network.add(ks.layers.Dense(2, activation='linear'))
 
-            neural_network.compile(optimizer=ks.optimizers.Adam(lr=0.001),
-                                   loss='mse')
-                    
-            env = NoisyCartPole(std= sigma, render=False)
-            actions = env.valid_actions()
-            
-            dqn = QNetworkSL(neural_network, actions, lambda x: np.reshape(x.state, newshape=(1, 4)),
-                             lambd=lambda_parameter,
-                             lambda_min=1e-3,
-                             gamma=1.0,
-                             reward_factor=0.01,
-                             fixed_length=100)
+    for sigma in sigmas:
 
-            dql = DeepSarsa(env, dqn,
-                            epsilon=1.0,
-                            epsilon_step_factor=0.9995,
-                            epsilon_min=0.0,
-                            replay_memory_size=1000
-            )
+        neural_network = ks.models.Sequential()
+        neural_network.add(ks.layers.Dense(150, activation='relu', input_shape=(4,)))
+        neural_network.add(ks.layers.Dense(50, activation='relu'))
+        neural_network.add(ks.layers.Dense(2, activation='linear'))
 
-            c = dql.get_configuration()
-            experiment = l.start_experiment( c )
-            q = dql.learn( num_episodes=episodes, result_handler=experiment.log)
-            experiment.save_attribute("weights", neural_network.get_weights())
-            print("%s finished sigma=%1.2f, run=%i" % (filename, sigma, run_n) )
+        neural_network.compile(optimizer=ks.optimizers.Adam(lr=0.001),
+                               loss='mse')
+
+        env = NoisyCartPole(std= sigma, render=False)
+        actions = env.valid_actions()
+
+        dqn = QNetworkSL(neural_network, actions, lambda x: np.reshape(x.state, newshape=(1, 4)),
+                         lambd=lambda_parameter,
+                         lambda_min=1e-3,
+                         gamma=1.0,
+                         reward_factor=0.01,
+                         fixed_length=100)
+
+        dql = DeepSarsa(env, dqn,
+                        epsilon=1.0,
+                        epsilon_step_factor=0.9995,
+                        epsilon_min=0.0,
+                        replay_memory_size=1000
+        )
+
+        c = dql.get_configuration()
+        experiment = l.start_experiment( c )
+        q = dql.learn( num_episodes=episodes, result_handler=experiment.log)
+        experiment.save_attribute("weights", neural_network.get_weights())
+        print("%s finished sigma=%1.2f, run=%i" % (filename, sigma, run_n) )
     return filename
 
 if __name__ == "__main__":
@@ -57,8 +57,8 @@ if __name__ == "__main__":
     sigmas = np.array([0, 10**-2, 10**-1, 10**-0])
     lambdas = np.array([0, 0.5, 0.75, 0.9, 1])
 
-    with Pool(processes=32) as pool:
-        for i in pool.starmap(experiment, [(runs, episodes, sigmas, l) for l in lambdas]):
+    with Pool(processes=64) as pool:
+        for i in pool.starmap(experiment, [(run_n, episodes, sigmas, l) for l in lambdas for run_n in range(runs)]):
             print("Finished %s" % i) 
     
     
