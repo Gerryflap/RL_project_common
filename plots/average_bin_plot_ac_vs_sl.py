75aef475b5d3af64a42541353ea1837301a1086b
gerryflap <gerry2211@gmail.com>
2018-06-25 12:29:46 +0200

    Renamed files

--- plots/average_bin_plot_ac_vs_sl.py
+++ plots/average_bin_plot_ac_vs_sl.py
@@ -7,7 +7,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-STARTS_WITH = "cartpole"
+STARTS_WITH = "AC_VS_SL"
 BIN_SIZE = 10
 
 if __name__ == "__main__":
@@ -22,49 +22,48 @@ if __name__ == "__main__":
                     #fig, ax = plt.subplots(nrows=1, ncols=1)
                     for result_name, result in session.items():
                         config = json.loads(result.attrs['configuration'])
-                        lambd, sigma = config["model"]["lambd"], config["env"]["std"]
-                        runs[(lambd, sigma)].append(result.value)
+                        print(config)
+                        a2c = config["self"] == 'ActorCriticAgent'
+                        runs[a2c].append(result.value)
                         #plt.plot(result.value)
                     #plt.title(fname)
                     #fig.savefig(fname + "_plot.png")
                     #plt.close(fig)
 
-    sigmas = [0, 0.01, 0.1, 1]
-    lambdas = [0, 0.5, 0.75, 0.9, 1]
-    sigma_colors = ["#0000FF", "#FF9900", "#00AA00", "#FF0000"]
+    a2c_vals = [False, True]
+    a2c_colors = ["#0000FF", "#FF9900"]
 
-    for lambd in lambdas:
-        # fig, ax = plt.subplots(nrows=1, ncols=1)
-        plt.title("Average scores for Deep SARSA(λ) with λ=%.2f"%lambd)
-        for sigma, color in zip(sigmas, sigma_colors):
-            results = runs[(lambd, sigma)]
-            merged = np.stack(results, axis=1)
-            merged = np.reshape(merged, (-1, BIN_SIZE, 5))
-            merged_2 = np.reshape(merged, (-1, BIN_SIZE * 5))
-            merged = np.mean(merged, axis=1)
-            stds = np.std(merged, axis=1, ddof=1)/np.sqrt(5)
-            # 50% confidence interval
-            stds *= 0.68
-            print(stds)
-            means = np.mean(merged, axis=1)
+    # fig, ax = plt.subplots(nrows=1, ncols=1)
+    plt.title("Average scores for Deep SARSA(λ) and A2C(λ)")
+    for a2c, color in zip(a2c_vals, a2c_colors):
+        results = runs[a2c]
+        merged = np.stack(results, axis=1)
+        merged = np.reshape(merged, (-1, BIN_SIZE, 5))
+        merged_2 = np.reshape(merged, (-1, BIN_SIZE * 5))
+        merged = np.mean(merged, axis=1)
+        stds = np.std(merged, axis=1, ddof=1)/np.sqrt(5)
+        # 50% confidence interval
+        stds *= 0.68
+        print(stds)
+        means = np.mean(merged, axis=1)
 
-            #sorted = np.sort(merged, axis=1)
-            #sorted_2 = np.sort(merged_2, axis=1)
-            #upper = sorted_2[:, 5]
-            #lower = sorted_2[:, -6]
+        #sorted = np.sort(merged, axis=1)
+        #sorted_2 = np.sort(merged_2, axis=1)
+        #upper = sorted_2[:, 5]
+        #lower = sorted_2[:, -6]
 
-            upper = means + stds
-            lower = means - stds
+        upper = means + stds
+        lower = means - stds
 
-            xs = np.arange(0, 250, BIN_SIZE)
+        xs = np.arange(0, 100, BIN_SIZE)
 
-            plt.fill_between(xs, upper, lower, color=color+"55")
-            plt.plot(xs, means, color=color+"FF")
-        plt.legend(sigmas, title="Noise σ before scaling")
-        plt.xlabel("Epsiode")
-        plt.ylabel("Score")
-        plt.savefig("../plots/plot_lambda_%.2f.png"%lambd, dpi=300)
-        plt.close()
+        plt.fill_between(xs, upper, lower, color=color+"55")
+        plt.plot(xs, means, color=color+"FF")
+    plt.legend(["SARSA(λ)", "A2C(λ)"], title="Agent")
+    plt.xlabel("Epsiode")
+    plt.ylabel("Score")
+    plt.savefig("../plots/plot_sl_vs_ac.png", dpi=300)
+    plt.close()
 
 
 
